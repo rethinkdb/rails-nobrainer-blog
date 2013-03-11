4889a51409a5c8a4f12b54abe5123eb310e8462f
Alex Popescu <the.mindstorm@gmail.com>
2013-03-11 14:47:57 -0700

    disabled ActiveRecord support

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Disable ActiveRecord support for relational databases
+#require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_resource/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -45,13 +51,17 @@ module Blog
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
+
+    # Commented out to disable ActiveRecord support for relational databases
     # config.active_record.schema_format = :sql
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+
+    # Commented out to disable ActiveRecord support for relational databases
+    #config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
