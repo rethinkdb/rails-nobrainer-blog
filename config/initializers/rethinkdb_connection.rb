99233a1dfcfaaccecc0e8cd6ee7f893eded77616
Alex Popescu <the.mindstorm@gmail.com>
2013-03-11 14:48:17 -0700

    RethinkDB connection initializer for Nobrainer

--- config/initializers/rethinkdb_connection.rb
+++ config/initializers/rethinkdb_connection.rb
@@ -1,7 +1,7 @@
 require 'nobrainer'
 
-# Connection initialized for +NoBrainer+. By default
-# it connects to +localhost:28015+, but the RethinkDB
-# instance can be specified using +RDB_URL+ environment
+# Connection details for +NoBrainer+ to connect to a running RethinkDB instance.
+# By default it connects to +localhost:28015+,
+# but a different connection can be specified using +RDB_URL+ environment
 # variable in the form: +rethinkdb://<host>:<port>/<db>+
 NoBrainer.connect ENV['RDB_URL'] || 'rethinkdb://localhost:28015/blog'
